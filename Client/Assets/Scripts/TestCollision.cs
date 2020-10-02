00040ea6d2c3e9d8f529e6060418d4b0d797a79b
RainMaker3363 <jsc22637@gmail.com>
2020-10-02 17:20:51 +0900

    My Work 2
    Client에서 TileMap 시스템(Tool 포함)을 추가했습니다.

---
+++ Client/Assets/Scripts/TestCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestCollision : MonoBehaviour
+{
+    public Tilemap _tilemap;
+    public TileBase _tileBase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tilemap.SetTile(new Vector3Int(0, 0, 0), _tileBase);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Vector3Int> blocked = new List<Vector3Int>();
+
+        foreach(var pos in _tilemap.cellBounds.allPositionsWithin)
+        {
+            var tile = _tilemap.GetTile(pos);
+            if (tile != null)
+                blocked.Add(pos);
+        }
+    }
+}
